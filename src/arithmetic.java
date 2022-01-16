cca22c2f9e0bc8e3e4d7e3384de9a1c0576f1d96
Quentin Hewitt <qhewitt@ymail.com>
2022-01-15 23:49:53 -0600

    morePractice

--- src/arithmetic.java
+++ src/arithmetic.java
@@ -52,4 +52,17 @@ public class arithmetic {
         return false;
       }
       
+      public String fizzBuzz(Integer i) {
+      String result = "";
+if (i % 3 == 0) {
+    result += "Fizz";
+}
+if (i % 5 == 0) {
+    result += "Buzz";
+}
+if (result.equals("")) {
+    result = i.toString();
+}
+return result;
+      }
 }
