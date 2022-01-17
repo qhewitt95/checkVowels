f6b224528fe21422034e311615e1996b8dcd70d2
Quentin Hewitt <qhewitt@ymail.com>
2022-01-16 23:47:18 -0600

    nodes

--- src/arithmetic.java
+++ src/arithmetic.java
@@ -73,4 +73,16 @@ return result;
       }
       return factorial;
     }
+
+    public Integer count(ListNode node) {
+    int count = 0;
+    ListNode current = node;
+    while (current != null) {
+        count++;
+        current = current.next();
+    }
+    return count;
+}
+
+
 }
