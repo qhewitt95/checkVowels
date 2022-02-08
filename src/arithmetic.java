7dfebcd6f852d123c6925a02460151e4807b66f0
Quentin Hewitt <qhewitt@ymail.com>
2022-02-08 17:42:09 -0600

    rememberarrays

--- src/arithmetic.java
+++ src/arithmetic.java
@@ -1,3 +1,5 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class arithmetic {
 
     public static void main(String[] args) {
@@ -128,6 +130,22 @@ for (int i : list) {
 
 }
 return maximum;
+
+
+String backpack[] = {"Shotgun", "Assualt Rifle", "Sniper"};
+String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+System.out.println("Backpack Items");
+System.out.println(backpack[0]);
+System.out.println(backpack[1]);
+System.out.println(backpack[2]);
+
+System.out.println("Theses are the zombies!");
+System.out.println(zombies[0]);
+System.out.println(zombies[0]);
+System.out.println(zombies[0]);
+
+System.out.println(backpack[2]);
+
 
 }
 
